9dbe49af6c7f8106cce523b4e34728df7e867ad8
q2533q <q2533q@gmail.com>
2021-11-01 09:10:10 +0800

    Create arm-install.sh

---
+++ x-ui/arm-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd /root/
+rm -rf x-ui/ /usr/local/x-ui/ /usr/bin/x-ui
+wget https://cdn.jsdelivr.net/gh/q2533q/files/x-ui/x-ui-linux-arm64.tar.gz
+tar zxvf x-ui-linux-amd64.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+cp x-ui/x-ui.sh /usr/bin/x-ui
+cp -f x-ui/x-ui.service /etc/systemd/system/
+mv x-ui/ /usr/local/
+systemctl daemon-reload
+systemctl enable x-ui
+systemctl restart x-ui
