10dc4b99605299e78667bb074ecece95faf2715d
q2533q <q2533q@gmail.com>
2023-11-23 20:24:12 +0800

    Update online.sh

--- runcode.io/online.sh
+++ runcode.io/online.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-	echo 3 > /proc/sys/vm/drop_caches
-	/usr/bin/python3 /home/ubuntu/runcode/run_user_script.py
-	sleep 300
+  echo 3 > /proc/sys/vm/drop_caches
+  /usr/bin/python3 /home/ubuntu/runcode/run_user_script.py
+  sleep 300
 done
